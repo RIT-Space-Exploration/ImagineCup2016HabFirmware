8a30cb2c3e2dbc54aa192930b798f5a3052b8d30
Austin Bodzas <austinbodzas@gmail.com>
2016-10-18 20:44:16 -0400

    Basic frame of ino program

---
+++ main.ino
@@ -0,0 +1,19 @@
+////////////////////////
+/// RIT SPEX HAB
+/// Main Flight Software
+/// October 20th, 2016
+/// 
+/// Austin Bodzas
+////////////////////////
+
+#include "Wire.h"
+#include "SPI.h"
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
